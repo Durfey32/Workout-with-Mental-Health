5dda206136b78959148f95595d8d5a4d1e9a10ca
Spencer_Durfey <durfey_32@yahoo.com>
2024-11-12 22:43:10 -0700

    added jwt

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import datetime
+from flask_jwt_extended import JWTManager
+from config import Config
 
 db = SQLAlchemy()
 ma = Marshmallow()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
     CORS(app)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -16,6 +19,7 @@ def create_app():
 
     db.init_app(app)
     ma.init_app(app)
+    jwt.init_app(app)
 
     with app.app_context():
         from .routes import user_bp
--- server/app/routes.py
+++ server/app/routes.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, jsonify
 from .models import User
 from . import db, ma
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 
 user_bp = Blueprint('user_bp', __name__)
 
@@ -12,13 +15,50 @@ user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
 @user_bp.route('/user', methods=['POST'])
+def signup():
+    name = request.json['name']
+    email = request.json['email']
+    password = request.json['password']
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({'message': 'User already exists'})
+    
+    hashed_password = generate_password_hash(password)
+    new_user = User(name, email, hashed_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return user_schema.jsonify(new_user), 201
+
+@user_bp.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email', None) 
+    password = request.json.get('password', None)
+
+    user = User.query.filter_by(email=email).first()
+
+    if user and check_password_hash(user.password, password):
+        expires = datetime.timedelta(hours=24)
+        access_token = create_access_token(identity=str(user.id), expires_delta=expires)
+        return jsonify({'token': access_token}), 200
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
+    
+@user_bp.route('/user', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    return jsonify(logged_in_as=user_schema.dump(user)), 200
+
+@user_bp.route('/user', methods=['POST'])
 def add_user():
     name = request.json['name']
     email = request.json['email']
     password = request.json['password']
 
     new_user = User(name, email, password)
-
     db.session.add(new_user)
     db.session.commit()
 
@@ -46,7 +86,6 @@ def update_user(id):
     user.name = name
     user.email = email
     user.password = password
-
     db.session.commit()
 
     return user_schema.jsonify(user)
