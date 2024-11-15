270d68fe7247e82ad3022f427e35b3eae846dd97
Tavin Sowell <tavinsowell@gmail.com>
2024-11-14 22:32:36 -0700

    api

--- server/app/api.py
+++ server/app/api.py
@@ -1,29 +1,11 @@
-from flask import request, jsonify, Flask
-from app import app
-from . import db
-from app.models import Workout
-from app.schemas import workout_schema
-
-
+from flask import Blueprint, request, jsonify
 import requests
-app = Flask(__name__)
 
-@app.route('/workout', methods=['POST'])
-def add_workout():
-    name = request.json['name']
-    type = request.json['type']
-    muscle = request.json['muscle']
-    equipment = request.json['equipment']
-    difficulty = request.json['difficulty']
-    instructions = request.json['instructions']
-    new_workout = Workout(name=name, type=type, muscle=muscle, equipment=equipment, difficulty=difficulty, instructions=instructions)
-    db.session.add(new_workout)
-    db.session.commit()
-    return workout_schema.jsonify(new_workout)
+api_bp = Blueprint('api_bp', __name__)
 
-@app.route('/exercises', methods=['GET'])
+@api_bp.route('/exercises', methods=['GET'])
 def get_exercises():
-    muscle = request.args.get('muscle')
+    muscle = request.args.get('muscle', 'biceps')
     api_url = 'https://api.api-ninjas.com/v1/exercises?muscle={}'.format(muscle)
     response = requests.get(api_url, headers={'X-Api-Key': 't79x9D05SpMMobuHZoL+3A==iigH52Z3n4indYPg'})
     
