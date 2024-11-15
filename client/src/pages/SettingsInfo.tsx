a5a0496594d94d424494bec754599fe4a2853cb1
SirMac <sir.mattew.holmes@gmail.com>
2024-11-14 20:59:50 -0700

    added react

--- client/src/pages/SettingsInfo.tsx
+++ client/src/pages/SettingsInfo.tsx
@@ -1,5 +1,13 @@
-const SettingsInfo = () => {
-    // Here will be settings page for the user and website
+import React from 'react';
+
+const SettingsInfo: React.FC = () => {
+  return (
+    <div className="settings-info">
+      <h2>About & Settings</h2>
+      <p>Learn more about the app, privacy settings, and user preferences.</p>
+      {/* Add settings functionality here */}
+    </div>
+  );
 };
 
 export default SettingsInfo;
