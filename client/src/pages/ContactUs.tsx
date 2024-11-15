b114b9de181aa7fc6bd3a327b9855fa1c3fde5f9
Spencer_Durfey <durfey_32@yahoo.com>
2024-11-15 13:02:17 -0700

    added functionallity to the package.json and the viteconfig

--- client/src/pages/ContactUs.tsx
+++ client/src/pages/ContactUs.tsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const ContactUs = () => {
     // This is a simple page that displays a contact information
+    return (
+      <div className="contact-us">
+        <h1>Contact Us</h1>
+        <p>For any questions or concerns, please contact us at:</p>
+<p>Github: 
+    <Link to="https://github.com/Durfey32">durfe32</Link>
+    <Link to="https://github.com/Tavinsowell">Tavinsowell</Link>
+    <Link to="https://github.com/b3hold23">b3hold23</Link>
+    <Link to="https://github.com/SirMac006">SirMac006</Link>
+</p>
+      </div>
+    );
 };
 
 export default ContactUs;
