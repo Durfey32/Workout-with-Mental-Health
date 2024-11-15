a25f3e2be84bf8e584d66fd6f1e8551d6c15da22
unknown <rosa.angel.daniel@gmail.com>
2024-11-14 19:37:14 -0700

    adding route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,12 @@ import './index.css'
 
 import App from './App.tsx'
 import ErrorPage from './pages/ErrorPage.tsx'
+import MainPage from './pages/MainPage.tsx'
+import CreateAccount from './pages/CreateAccount.tsx'
+import Login from './pages/Login.tsx'
+import Home from './pages/Home.tsx'
+import Profile from './pages/Profile.tsx'
+import ContactUs from './pages/ContactUs.tsx'
 
 
 createRoot(document.getElementById('root')!).render([
@@ -29,6 +35,14 @@ createRoot(document.getElementById('root')!).render([
         {
           path: 'home',
           element: <Home />
+        },
+        {
+          path: 'profile',
+          element: <Profile />
+        },
+        {
+          path: 'contact-us',
+          element: <ContactUs />
         },
       ]
     }
