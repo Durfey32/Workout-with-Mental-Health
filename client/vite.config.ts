3a814f8cb06a3f9cd06260b32388a9e408479db5
unknown <rosa.angel.daniel@gmail.com>
2024-11-15 22:26:45 -0700

    merging with main

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,24 +1,8 @@
 import { defineConfig } from 'vite';
-<<<<<<< HEAD
-=======
 import react from '@vitejs/plugin-react';
->>>>>>> 9be4df3d02cf10d0c26d98e55135877229029640
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-       target: 'http://localhost:3001',
-       changeOrigin: true,
-       secure: false,
-    },
-  },
-},
-=======
   plugins: [react()],
   server: {
     port: 3000,
@@ -37,5 +21,4 @@ export default defineConfig({
       },
     },
   },
->>>>>>> 9be4df3d02cf10d0c26d98e55135877229029640
 });
