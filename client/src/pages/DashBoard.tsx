55604b95a1fa704a0c52a1b328484322d7489ddf
Spencer_Durfey <durfey_32@yahoo.com>
2024-11-15 20:32:51 -0700

    quotes on main page

--- client/src/pages/DashBoard.tsx
+++ client/src/pages/DashBoard.tsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-const DashBoard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
-=======
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -29,7 +21,6 @@ const DashBoard: React.FC = () => {
       <p>Track your progress, access workouts, and Journal!</p>
     </div>
   );
->>>>>>> 9be4df3d02cf10d0c26d98e55135877229029640
 };
 
 export default DashBoard;
