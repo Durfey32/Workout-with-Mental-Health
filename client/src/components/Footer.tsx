9fd7186ef33e17f8678c61f0466d8dcddd67c8a4
Spencer_Durfey <durfey_32@yahoo.com>
2024-11-25 11:41:16 -0700

    render api

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer bg-dark text-white py-3">
       <div className="container text-center">
         <p className="mb-2">
-          Made by: Spencer Durfey, Tavin Sowell, Angel Fernandez, and Matthew Holmes
+          Spencer Durfey, Tavin Sowell, Angel Fernandez, and Matthew Holmes
         </p>
         <Link to="/contact-us" className="text-info text-decoration-none">
           Contact Us
